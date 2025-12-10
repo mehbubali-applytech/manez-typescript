b52a585396f69dee581b9e00e1f42310e356cdf2
mehbubali-applytech <mehbub.ali@appplytech.com>
2025-12-10 17:21:47 +0530

    Update

--- src/components/elements/advanced-ui/steps/StepsMainArea.tsx
+++ src/components/elements/advanced-ui/steps/StepsMainArea.tsx
@@ -1,15 +1,24 @@
 import Breadcrumb from '@/common/Breadcrumb/breadcrumb';
-import React from 'react';
+import React, { useState } from 'react';
 import CircleStepWithIcon from './CircleStepWithIcon';
 import CircleStepWithNumber from './CircleStepWithNumber';
 import BoxStepWithNumber from './BoxStepWithNumber';
-import BoxStepWithIcon from './BoxStepWithIcon';
+import BoxStepWithIcon, { StepItem } from './BoxStepWithIcon';
 import FourBoxStep from './FourBoxStep';
 import BoxStepWithVariation from './BoxStepWithVariation';
 import TextSteps from './TextSteps';
 import VerticalTextSteps from './VerticalTextSteps';
 
 const StepsMainArea = () => {
+const steps: StepItem[] = [
+  { step: 1, title: "Company Info", icon: "fa-solid fa-building" },
+  { step: 2, title: "Primary Contact", icon: "fa-solid fa-user" },
+  { step: 3, title: "Settings", icon: "fa-solid fa-sliders" },
+  { step: 4, title: "Modules", icon: "fa-solid fa-layer-group" },
+];
+
+const [currentStep, setCurrentStep] = useState(0);
+
     return (
         <>
             {/* -- App side area start -- */}
@@ -26,7 +35,12 @@ const StepsMainArea = () => {
                         <BoxStepWithNumber/>
                     </div>
                     <div className="col-span-12">
-                        <BoxStepWithIcon/>
+                        <BoxStepWithIcon
+  steps={steps}
+  currentStep={currentStep}
+  onStepChange={setCurrentStep}
+/>
+
                     </div>
                     <div className="col-span-12">
                        <FourBoxStep/>
